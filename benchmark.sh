5e27bd19a14fd24efcb67df13aa51fd0d6da0cee
Mondotosz <git@moncorp.ch>
2024-09-30 10:42:31 +0200

    updated benchmark script to ignore execution status

--- benchmark.sh
+++ benchmark.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 # Build package
-if ./mvnw package; then
-	exit 1
-fi
+mvn package
 
 JAR=target/java-ios-1.0-SNAPSHOT.jar
 FILE=/tmp/file.bin
 OUT=benchmark.txt
+SIZE=1048576
 
 date >$OUT
-java -jar $JAR $FILE -i TEXT write -s 1024 >>$OUT
+java -jar $JAR $FILE -i TEXT write -s $SIZE >>$OUT
 du -hb $FILE >>benchmark.txt
 java -jar $JAR $FILE -i TEXT read >>$OUT
-java -jar $JAR $FILE -i BUFFERED_TEXT write -s 1024 >>$OUT
+java -jar $JAR $FILE -i BUFFERED_TEXT write -s $SIZE >>$OUT
 du -hb $FILE >>$OUT
 java -jar $JAR $FILE -i BUFFERED_TEXT read >>$OUT
-java -jar $JAR $FILE -i BINARY write -s 1024 >>$OUT
+java -jar $JAR $FILE -i BINARY write -s $SIZE >>$OUT
 du -hb $FILE >>$OUT
 java -jar $JAR $FILE -i BINARY read >>$OUT
-java -jar $JAR $FILE -i BUFFERED_BINARY write -s 1024 >>$OUT
+java -jar $JAR $FILE -i BUFFERED_BINARY write -s $SIZE >>$OUT
 du -hb $FILE >>$OUT
 java -jar $JAR $FILE -i BUFFERED_BINARY read >>$OUT
